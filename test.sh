c7398f6e6d81e05492886b11649b3f5b5ca433ec
wat-aro <kazutas1008@gmail.com>
2022-05-06 23:56:42 +0900

    Create a compiler to compile a single integer

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+success() {
+  echo -e "\e[32m$1\e[m"
+}
+
+failure() {
+  echo -e "\e[31m$1\e[m"
+}
+
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./9cc "$input" > tmp.s
+  if [ "$?" != 0 ]; then
+    exit 1
+  fi
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    success "$input => $actual"
+  else
+    failure "$input => $expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
+
+success OK
